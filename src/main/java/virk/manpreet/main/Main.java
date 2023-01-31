84921f533b97ac43ef93ee9bc4d1331ae8bb25ab
sukhm <sukhm@DESKTOP-GOLDU2O.mshome.net>
2023-01-31 17:40:33 -0500

    Java In class Assignment moved commented out code to the bottom

--- src/main/java/virk/manpreet/main/Main.java
+++ src/main/java/virk/manpreet/main/Main.java
@@ -11,27 +11,10 @@ package virk.manpreet.main;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        /*
-        System.out.printf("%s%n%s%n", "Hello World","Whats up");
-        double PI = 3.14159;
-        
-        System.out.printf("Number PI is %.3f\n",PI);
-        */
+      
         Scanner input = new Scanner(System.in);
         
-/*
-        System.out.print("Enter first integer:");
-        int number1 = input.nextInt();
-        System.out.print("Enter second integer:");
-        int number2 = input.nextInt();
-        System.out.print("Enter third integer:");
-        int number3 = input.nextInt();
-        System.out.print("Enter fourth integer:");
-        int number4 = input.nextInt();
-        System.out.print("Enter fifth integer:");
-        int number5 = input.nextInt();
-        System.out.print("These are the 5 number the user input\n" +  number1 + number2 + number3 + number4 + number5);
-*/
+
         System.out.print("Enter a five digit number:");
         int num = input.nextInt();
         int num1 = num / 10000;
@@ -45,9 +28,26 @@ public class Main {
                   System.out.printf("%3s",num4);
                   System.out.printf("%3s",num5);
                   
-
+  /*
+        System.out.printf("%s%n%s%n", "Hello World","Whats up");
+        double PI = 3.14159;
         
+        System.out.printf("Number PI is %.3f\n",PI);
+        */
         
+        /*
+        System.out.print("Enter first integer:");
+        int number1 = input.nextInt();
+        System.out.print("Enter second integer:");
+        int number2 = input.nextInt();
+        System.out.print("Enter third integer:");
+        int number3 = input.nextInt();
+        System.out.print("Enter fourth integer:");
+        int number4 = input.nextInt();
+        System.out.print("Enter fifth integer:");
+        int number5 = input.nextInt();
+        System.out.print("These are the 5 number the user input\n" +  number1 + number2 + number3 + number4 + number5);
+*/
   
        
      } 
